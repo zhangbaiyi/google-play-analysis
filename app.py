da8b7c3b281fd523679d15a16e7c1459d00eb913
Baiyi Zhang <100baiyi@gmail.com>
2023-12-06 22:41:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ import dash
 import pandas as pd
 from dash import html, dcc
 import plotly.express as px
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 df = pd.read_csv('output/visualization_dash.csv')
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 def serve_layout():
     return html.Div([
-        html.H3("App Data Visualization", style={'textAlign': 'center'}),
+        html.H3("Play Store Insights", style={'textAlign': 'center'}),
         dcc.Tabs(id="tabs", value='tab-1', children=[
-            dcc.Tab(label='Tab 1', value='tab-1', children=[
+            dcc.Tab(label='Cube', value='tab-1', children=[
                 html.Div(id='output-div'),
-                dcc.Graph(id='main-graph', style={'height': '800px'}),
+                dcc.Graph(id='main-graph'),
                 html.H3("Select Category:", style={'marginTop': '20px'}),
                 dcc.Dropdown(
                     id='category-dropdown',
@@ -61,7 +62,10 @@ def serve_layout():
                 ),
                 html.Br(),
             ]),
-            dcc.Tab(label='Tab 2', value='tab-2'),
+            dcc.Tab(label='Resources', value='tab-2', children=[
+                html.Button("Download Dataset", id="download-button"),
+                dcc.Download(id="download-dataset")
+            ]),
         ]),
     ], style={'padding': '20px'})
 
@@ -80,20 +84,27 @@ app.layout = serve_layout
 def update_graph(selected_categories, selected_content_rating, free_paid, app_age_range, size):
     filtered_df = df[df['Category'].isin(selected_categories) &
                      (df['Content Rating'].isin(selected_content_rating)) &
-                     (df['Free'].astype(str) == free_paid) &
+                     (df['Free'] == free_paid) &
                      (df['App Age'] >= app_age_range[0]) &
                      (df['App Age'] <= app_age_range[1]) &
                      (df['Size in MB'] <= size)]
 
     # Generating the graph
-    fig = px.scatter_3d(filtered_df, x='Rating', y='Size in MB', z='Installs', color='Category', size='Installs',)
-    fig.update_layout(height=800)
-
-
-    # Output message
+    fig = px.scatter_3d(filtered_df, x='Rating', y='Rating Count', z='Installs', color='Category', size = 'Installs')
+    fig.update_layout(height=1000)
     output_message = f"Displaying data for {len(filtered_df)} apps"
-
     return fig, output_message
+
+
+@app.callback(
+    Output("download-dataset", "data"),
+    Input("download-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def download_data(n_clicks):
+    if n_clicks is None:
+        raise PreventUpdate
+    return dcc.send_data_frame(df.to_csv, "visualization_dash.csv")
 
 
 if __name__ == '__main__':
